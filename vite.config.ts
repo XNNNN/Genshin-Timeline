204c09f432114d4edb47ce2260531b24198e2052
zhanghao <zhanghao@csdn.net>
2022-05-31 16:18:55 +0800

    删除文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,47 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from "path"
-import { changePackageVersion } from "./build/plugins"
-import { readdirSync } from 'fs';
+import { resolve } from 'path'
+import { changePackageVersion } from './build/plugins'
+import { readdirSync } from 'fs'
 
 /**
  * 获取多入口文件
- * @returns 
+ * @returns
  */
 export function getPages() {
-  let pagePath = resolve(__dirname, "./src/pages");
-  let files: string[] = readdirSync(pagePath);
+  let pagePath = resolve(__dirname, './src/pages')
+  let files: string[] = readdirSync(pagePath)
   let pages: { [key: string]: string } = {
-    main: resolve(__dirname, 'index.html')
-  };
-  for (let i = 0; i < files.length; i++) {
-    let key = files[i].replace('.html', '');
-    if (key === 'index') continue;
-    pages[key] = resolve(__dirname, `src/pages/${files[i]}`);
+    main: resolve(__dirname, 'index.html'),
   }
-  return pages;
+  for (let i = 0; i < files.length; i++) {
+    let key = files[i].replace('.html', '')
+    if (key === 'index') continue
+    pages[key] = resolve(__dirname, `src/pages/${files[i]}`)
+  }
+  return pages
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: './',
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src")
-    }
+      '@': resolve(__dirname, './src'),
+    },
   },
   server: {
-    host: process.env.NODE_ENV !== "production"
+    host: process.env.NODE_ENV !== 'production',
   },
   plugins: [
     changePackageVersion(),
     vue({
-      refTransform: [/src/]
-    })
+      refTransform: [/src/],
+    }),
   ],
   build: {
     rollupOptions: {
       input: getPages(),
-    }
-  }
+    },
+  },
 })
